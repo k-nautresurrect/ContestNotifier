de34002c823e6fa1881e37bcdaab72c98359b90a
k-nautresurrect <kushignite@gmail.com>
2022-02-19 20:56:41 +0530

    Created Table

--- script/main.js
+++ script/main.js
@@ -1,32 +1,29 @@
-const URL = "https://kontests.net/api/v1/all/"
-async function required(){
-    const response = await fetch(URL)
-    const users = await response.json()
-    console.log(users[2].name)
-    for (let i = 0; i<users.length;i++){
-        if (users[i].in_24_hours = 'YES'){
-            const name = users[i].name
-            const url = users[i].url
-            const element = document.createElement('div')
-            const matter = document.createTextNode(name)
-            var a = document.createElement('a')
-            var link = document.createTextNode('LINK')
-            a.appendChild(link)
-            a.title = 'LINK'
-            a.href = url
-            
-            element.appendChild(matter)
-            
-            const list1 = document.getElementById("div_id")
-            list1.appendChild(element)
-            list1.appendChild(a)
-    }
-    
-        // const div_section = document.createElementbyID('div')
-        // div_section.setAttribute('id','div_id')
-        // div_section.innerHTML = '<h1>name</h1><br><button>url</button><br>'
-    }
+const URL = "https://kontests.net/api/v1/all/";
+async function required() {
+  const response = await fetch(URL);
+  const users = await response.json();
+  for (let i = 0; i < users.length; i++) {
+    if ((users[i].in_24_hours = "YES")) {
+      const name = users[i].name;
+      const url = users[i].url;
 
+      const upComing = document.createElement("tr");
+      upComing.className = "upcoming";
+
+      var a = document.createElement("a");
+      var link = document.createTextNode("visit");
+      a.appendChild(link);
+      a.title = "visit";
+      a.href = url;
+      a.className = "links text-dark";
+      a.innerHTML = name;
+
+      upComing.innerHTML = `<td><a href="${url}" title="visit" class="links text-dark">${name}</a></td>`;
+
+      const list1 = document.getElementById("div_id");
+      list1.appendChild(upComing);
+    }
+  }
 }
 
-required()
+required();

f53d0c9b2b0ce594a5e78ede99fb1d5fc72df395
meghagarg23 <megha.garg.cs.2019@miet.ac.in>
2022-04-05 16:59:52 +0530

    Added Options

--- script/main.js
+++ script/main.js
@@ -41,7 +41,75 @@ async function fetchall() {
   }
 }
 
+function fetchviadata(data) {
+  for (let i = 0; i < data.length; i++) {
+    const name = data[i].name;
+    const url = data[i].url;
 
+    const upComing = document.createElement("tr");
+    upComing.className = "upcoming";
+
+    var a = document.createElement("a");
+    var link = document.createTextNode("visit");
+    a.appendChild(link);
+    a.title = "visit";
+    a.href = url;
+    a.className = "links text-dark";
+    a.innerHTML = name;
+
+    upComing.innerHTML = `<td><a href="${url}" title="visit" class="links text-dark">${name}</a></td>`;
+
+    const list1 = document.getElementById("div_id");
+    list1.appendChild(upComing);
+  }
+}
+
+async function fetchLink(str) {
+  const list = document.getElementById("div_id");
+  list.innerHTML = "";
+  let op = "";
+
+  details.innerHTML = `<h1 class="text-center">${str}</h1>`;
+  if (str === "All Contests") {
+    op = "all";
+  } else if (str === "Code Forces") {
+    op = "codeforces";
+  } else {
+    op =
+      str.split(" ")[0].toLowerCase() + "_" + str.split(" ")[1].toLowerCase();
+  }
+  const URL = `https://kontests.net/api/v1/${op}/`;
+  const response = await fetch(URL);
+  data = await response.json();
+  if (data) {
+    fetchviadata(data);
+  }
+}
+
+home.addEventListener("click", () => {
+  return fetchall();
+});
+hackerrank.addEventListener("click", () => {
+  fetchLink("Hacker Rank");
+});
+codechef.addEventListener("click", () => {
+  fetchLink("Code Chef");
+});
+codeforces.addEventListener("click", () => {
+  fetchLink("Code Forces");
+});
+atcoder.addEventListener("click", () => {
+  fetchLink("At Coder");
+});
+
+// search.addEventListener("input", (e) => {
+//   const value = e.target.value;
+//   searchData.forEach((contest) => {
+//     const isShown =
+//       contest.name.includes(value) || contest.site.includes(value);
+
+//   });
+// });
 
 fetchall();
 
